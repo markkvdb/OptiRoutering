8c083f1a0735356236118e8a152a0d1c0d14efa3
markkvdb <github@mvanderbroek.com>
2023-09-03 15:15:41 +0200

    Change select route definition UX

--- frontend/src/routes/problems/+page.svelte
+++ frontend/src/routes/problems/+page.svelte
@@ -5,20 +5,21 @@
 		const element = event.currentTarget as HTMLSelectElement;
 		const index = element.value;
 
-		if (index == 'new') {
-			window.location.href = '/problems/new';
-		} else {
-			window.location.href = `/problems/` + index;
-		}
+		if (index == '') return;
+
+		window.location.href = `/problems/` + index;
 	}
 </script>
 
 <h2>Select definition</h2>
 
 <!-- select existing route definition or create new one -->
+Select an existing route or create a new one:
 <select on:change={openPage}>
-	<option value="new">Create new route definition</option>
+	<option value="" selected>Select a route definition</option>
 	{#each data.route_definitions as route_definition, i}
 		<option value={i}>{route_definition.depot.address}</option>
 	{/each}
 </select>
+
+<button on:click={() => (window.location.href = '/problems/new')}>New</button>
