e2d3c150162f6fe89cb3c387c6b70cc4e23b47ff
markkvdb <github@mvanderbroek.com>
2023-09-03 19:50:15 +0200

    Add endpoint for route definition

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,57 @@
 """Entrypoint to the web application."""
 
+from datetime import time
+from uuid import uuid4
+
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Coordinates(BaseModel):
+    lat: float
+    lng: float
+
+class Location(BaseModel):
+    address: str
+    coordinates: Coordinates
+
+class Customer(BaseModel):
+    name: str
+    location: Location
+    demand: int | None = None
+    earliest_start: time | None = None
+    latest_end: time | None = None
+
+class BaseRouteDefinition(BaseModel):
+    depot: Location
+    customers: list[Customer]
+    maximum_capacity: int | None = None
+    time_windows: bool = False
+
+class RouteDefinitionRequest(BaseRouteDefinition):
+    pass
+
+class RouteDefinition(BaseRouteDefinition):
+    id: str
 
 app = FastAPI()
 
+route_definitions: dict[str, RouteDefinition] = {}
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+
+@app.post("/route")
+async def route(route_request: RouteDefinitionRequest) -> RouteDefinition:
+    """Calculate the optimal route for the given route definition."""
+    route_definition = RouteDefinition(
+        id=uuid4().hex,
+        **route_request.dict()
+    )
+    route_definitions[route_definition.id] = route_definition
+
+    return route_definition
+
+@app.get("/route/{id}")
+async def route(id: str) -> RouteDefinition:
+    """Get the route definition for the given route id."""
+    return route_definitions[id]
+
