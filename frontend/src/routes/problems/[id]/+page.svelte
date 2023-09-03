f44b3a6304231f741cf5c0f926f5d81273b7eb4f
markkvdb <github@mvanderbroek.com>
2023-09-03 14:42:50 +0200

    Clean up show page

--- frontend/src/routes/problems/[id]/+page.svelte
+++ frontend/src/routes/problems/[id]/+page.svelte
@@ -2,16 +2,15 @@
 	export let data;
 </script>
 
-<h2>{data.route_definition}</h2>
+<h2>{data.route_definition.depot.address}</h2>
 
 <h3>Depot</h3>
 
 <p>Address: {data.route_definition.depot.address}</p>
-<p>Coordinates: {data.route_definition.depot.coordinates}</p>
 
 <h3>Customers</h3>
 
 {#each data.route_definition.customers as customer}
+	<h4>{customer.name}</h4>
 	<p>Address: {customer.location.address}</p>
-	<p>Coordinates: {customer.location.coordinates}</p>
 {/each}
