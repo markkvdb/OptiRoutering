080443f29b58597c34133e0faa87a4c27d25ef43
markkvdb <github@mvanderbroek.com>
2023-09-17 23:25:50 +0200

    Add directions for optimal route

--- frontend/src/routes/problems/[id]/SolutionPath.svelte
+++ frontend/src/routes/problems/[id]/SolutionPath.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import type { RouteDefinition } from '$lib/api/models/routes';
+	import { Valhalla } from '@routingjs/valhalla';
 	import { GeoJSON } from 'svelte-leafletjs';
 
 	export let solution: number[] | undefined;
@@ -11,26 +12,29 @@
 	};
 
 	$: if (solution) {
-		path_geojson.features = [
-			{
-				type: 'Feature',
-				properties: {},
-				geometry: {
-					type: 'LineString',
-					coordinates: [
-						[route_definition.depot.coordinates.lng, route_definition.depot.coordinates.lat],
-						...solution.map(
-							(customer_id) =>
-								[
-									route_definition.customers[customer_id - 1].location.coordinates.lng,
-									route_definition.customers[customer_id - 1].location.coordinates.lat
-								] as GeoJSON.Position
-						),
-						[route_definition.depot.coordinates.lng, route_definition.depot.coordinates.lat]
-					]
-				}
-			}
-		];
+		const v = new Valhalla({
+			userAgent: null
+		});
+		v.directions(
+			[
+				[route_definition.depot.coordinates.lat, route_definition.depot.coordinates.lng],
+				...solution.map(
+					(customer_id) =>
+						[
+							route_definition.customers[customer_id - 1].location.coordinates.lat,
+							route_definition.customers[customer_id - 1].location.coordinates.lng
+						] as GeoJSON.Position
+				),
+				[route_definition.depot.coordinates.lat, route_definition.depot.coordinates.lng]
+			],
+			'auto'
+		).then((response) => {
+			const features = response.directions.map((direction) => {
+				return direction.feature;
+			});
+			console.log(features);
+			path_geojson.features = features;
+		});
 	}
 </script>
 
